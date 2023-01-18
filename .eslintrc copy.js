2bead5157d3b936445391fe7177538f536b800c9
Lautaro Nievas <lautynievas09@gmail.com>
2023-01-18 15:28:16 -0300

    first commit

---
+++ .eslintrc copy.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "standard",
+    "plugin:cypress/recommended",
+    "eslint-config-prettier",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "cypress"],
+  rules: {
+    "react/prop-types": "off",
+    curly: ["error", "all"],
+    camelcase: "off",
+  },
+};
